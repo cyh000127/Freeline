d42e3e89c0586affe0847d5b68af2cc8e3c463c2
okccc5 <okccc5@naver.com>
2026-03-09 17:20:39 +0900

    feat: 가이드 API 추가
    - Admin 생성 api 추가

---
+++ be/Freeline/src/main/java/com/freeline/domain/eventadmin/converter/EventAdminConverter.java
@@ -0,0 +1,29 @@
+package com.freeline.domain.eventadmin.converter;
+
+import com.freeline.domain.eventadmin.dto.request.EventAdminCreateReqDto;
+import com.freeline.domain.eventadmin.dto.response.EventAdminResDto;
+import com.freeline.domain.eventadmin.entity.EventAdmin;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class EventAdminConverter {
+
+	public EventAdmin toEntity(final EventAdminCreateReqDto dto, final String encodedPassword) {
+		return EventAdmin.builder()
+			.email(dto.email())
+			.password(encodedPassword)
+			.name(dto.name())
+			.build();
+	}
+
+	public EventAdminResDto toEventAdminResDto(final EventAdmin eventAdmin) {
+		return EventAdminResDto.builder()
+			.adminId(eventAdmin.getId())
+			.email(eventAdmin.getEmail())
+			.name(eventAdmin.getName())
+			.createdAt(eventAdmin.getCreatedAt())
+			.build();
+	}
+}
+
---
+++ be/Freeline/src/main/java/com/freeline/domain/eventadmin/dto/response/EventAdminResDto.java
@@ -0,0 +1,26 @@
+package com.freeline.domain.eventadmin.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+public record EventAdminResDto(
+
+	@Schema(description = "관리자 ID", example = "1")
+	Long adminId,
+
+	@Schema(description = "이메일", example = "admin@freeline.com")
+	String email,
+
+	@Schema(description = "관리자 이름", example = "최홍권")
+	String name,
+
+	@Schema(description = "생성 시각")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	LocalDateTime createdAt
+) {}
+
