0645503cb595bfc3794d87f4ff641f05768ef923
okccc5 <okccc5@naver.com>
2026-03-09 16:44:48 +0900

    chore: error, log, exception 관리 설정 추가

---
+++ be/Freeline/src/main/java/com/freeline/common/error/exception/BusinessException.java
@@ -0,0 +1,16 @@
+package com.freeline.common.error.exception;
+
+import com.freeline.common.error.ErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+	private final ErrorCode errorCode;
+
+	public BusinessException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
+
+}
