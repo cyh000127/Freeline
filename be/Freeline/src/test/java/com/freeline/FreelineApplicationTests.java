8a5a5a6d0047e720ec3256c26abcc9b6139f5fc9
SeunghunKang <kangseunghun9927@gmail.com>
2026-03-17 01:19:09 +0000

    chore: resolve rebase conflicts

--- be/Freeline/src/test/java/com/freeline/FreelineApplicationTests.java
+++ be/Freeline/src/test/java/com/freeline/FreelineApplicationTests.java
@@ -1,11 +1,16 @@
 package com.freeline;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 //@SpringBootTest
 //! 해당 테스트의 SonarQube issue는 처리 안하셔도 됩니다.
+
 @ActiveProfiles("local")
+@SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class FreelineApplicationTests {
 
     @Test
