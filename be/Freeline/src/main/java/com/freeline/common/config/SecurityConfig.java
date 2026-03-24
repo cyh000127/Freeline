62a0a13d429a29c933bf591b8731de04dfbc722f
typ0squir <typ0squir@gmail.com>
2026-03-24 13:09:34 +0900

    fix: 아이디 유효성 검사 권한 확대

--- be/Freeline/src/main/java/com/freeline/common/config/SecurityConfig.java
+++ be/Freeline/src/main/java/com/freeline/common/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
                                     "/api/v1/auth/email/**",
                                     "/api/v1/auth/refresh",
                                     "/api/v1/auth/booth-login",
-                                    "/api/v1/auth/visitor-login"
+                                    "/api/v1/auth/visitor-login",
+                                    "/api/v1/auth/check-id"
                             ).permitAll()
                             .requestMatchers(
                                     "/swagger-ui/**",
