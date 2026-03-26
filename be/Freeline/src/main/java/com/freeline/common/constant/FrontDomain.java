7c9bc1f93d899d39a673e5e54460f35793905fd9
okccc5 <okccc5@naver.com>
2026-03-26 10:29:07 +0900

    chore : 8081 포트 추가

--- be/Freeline/src/main/java/com/freeline/common/constant/FrontDomain.java
+++ be/Freeline/src/main/java/com/freeline/common/constant/FrontDomain.java
@@ -10,6 +10,7 @@ public enum FrontDomain {
 
     LOCAL1("http://localhost:3000", "프론트 로컬 도메인"),
     LOCAL2("http://localhost:3001", "프론트 로컬 도메인"),
+    LOCAL3("http://localhost:8081", "프론트 로컬 도메인"),
     LIVE("https://j14a207.p.ssafy.io", "프론트 배포 도메인");
 
     private final String url;
