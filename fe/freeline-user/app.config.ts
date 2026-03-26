15a14620c21f3152baf3f2db75436f4dbf9db5b7
okccc5 <okccc5@naver.com>
2026-03-27 04:00:36 +0900

    feat: EAS 설정

--- fe/freeline-user/app.config.ts
+++ fe/freeline-user/app.config.ts
@@ -1,7 +1,8 @@
 import type { ConfigContext, ExpoConfig } from 'expo/config';
 
 const API_BASE_URL = process.env.API_URL ?? 'https://j14a207.p.ssafy.io/api/v1';
-const EAS_PROJECT_ID = process.env.EAS_PROJECT_ID ?? '';
+const EAS_PROJECT_ID =
+  process.env.EAS_PROJECT_ID ?? 'fd7bcea8-fc0c-472a-b727-f42b1795c5a9';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
